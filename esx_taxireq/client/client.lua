e0e7270852a6544625462d0c46511a97c8250aac
Mani Khoshnood <81551013+theMani-kh@users.noreply.github.com>
2021-11-26 12:54:39 +0330

    Update client.lua

--- esx_taxireq/client/client.lua
+++ esx_taxireq/client/client.lua
@@ -57,34 +57,66 @@ end)
 
 function OpenReqsList()
 	ESX.TriggerServerCallback('mani_taxi:getReqs', function(reqs)
-		local elements = {
-			head = {"ShakHs", "ReQ ID", "ToziHaT", "StaTuS", "GoziNe"},
-			rows = {}
-		}
+		local elements = {}
 		for i=1, #reqs, 1 do
-			table.insert(elements.rows, {
-				data = reqs[i],
-				cols = {
-					reqs[i].name,
-					reqs[i].reqid,
-					reqs[i].reason,
-					reqs[i].status,
-					'{{' .. "Answer" .. '|answer}} {{' .. "Close" .. '|close}} {{' .. "Call" .. '|call}}'
-				}
+			table.insert(elements, {
+				label = "Request Id : "..reqs[i].reqid.." | Accept : "..reqs[i].status,
+				icname = reqs[i].name,
+				reqid = reqs[i].reqid,
+				text = reqs[i].reason,
+				status = reqs[i].status,
+				phone = reqs[i].phone,
+				id = reqs[i].id,
+				coord = reqs[i].coord,
+				accept = reqs[i].accept,
 			})
 		end
-		ESX.UI.Menu.Open('list', GetCurrentResourceName(), "reqs_lists", elements, function(data, menu)
-			local req = data.data
-			if data.value == 'answer' then
-				TriggerServerEvent('mani_taxi:areqs', req.reqid)
-				menu.close()
-			elseif data.value == 'close' then
-				TriggerServerEvent('mani_taxi:creqs', req.reqid)
-				menu.close()
-			elseif data.value == 'call' then
-				menu.close()
-				TriggerEvent('gcphone:autoCall', req.phone)
-			end
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'reqs_lists', {
+			title    = "Requests",
+			align    = 'top-left',
+			elements = elements
+		}, function(data, menu)
+			local elements = {}
+			local id = data.current.reqid
+			ESX.TriggerServerCallback('mani_taxi:acceptername', function(acceptername)
+				ESX.TriggerServerCallback('mani_taxi:icname', function(name)
+					table.insert(elements,{label = "Id : ".. data.current.reqid ,value = "nil"})
+					table.insert(elements,{label = "Accept status : "..data.current.status ,value = "nil"})
+					if data.current.accept == "open" then
+						table.insert(elements,{label = "Accept", value = "yes"})
+					else
+						table.insert(elements,{label = "Accepted by : ".. acceptername, value = "nil"})
+					end
+					if acceptername == name then
+						table.insert(elements,{label = "Finish",value = "finish"})
+					end
+					table.insert(elements,{label = "Pin location",value = "loc"})
+					table.insert(elements,{label = "Call",value = "call"})
+					ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'reqs_list', {
+						title    = "Request",
+						align    = 'top-left',
+						elements = elements
+						}, function(data2, menu2)
+						menu2.close()
+						if data2.current.value == 'yes' then
+							TriggerServerEvent('mani_taxi:areqs', data.current.reqid)
+							menu.close()
+						elseif data2.current.value == 'call' then
+							TriggerEvent('gcphone:autoCall', data.current.phone)
+							menu.close()
+						elseif data2.current.value == 'finish' then
+							TriggerServerEvent("mani_taxi:creqs", data.current.reqid)
+							menu.close()
+						elseif data2.current.value == 'loc' then
+							local coord = data.current.coord
+							SetNewWaypoint(coord.x, coord.y)
+							menu.close()
+						end
+					end, function(data2, menu2)
+						menu2.close()
+					end)
+				end)
+			end, id)
 		end, function(data, menu)
 			menu.close()
 		end)
